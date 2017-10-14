3994f8b68d04309865379f8b22785e1836cfdec4
Andres19920706 <arp00048@red.ujaen.es>
2017-10-14 21:48:39 +0200

    fin ejemplo 01

---
+++ Arduino_en_Java/Ejercicio01/CodigoArduino/EjemploDeTransmisionEnJava/sketch_oct14a/sketch_oct14a.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // Pin 13 como salida
+  pinMode(13,OUTPUT);
+  // Establecemos la comunicación en el puerto serie
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // Si hay informaicón en el puerto serial, capturamos los datos
+  if (Serial.available() > 0){
+    //Leemos la información que hay en el puerto serial
+    int input = Serial.read();
+    if(input=='1'){
+        //Si el valor es 1, encendemos el Led
+        digitalWrite(13, HIGH);
+      }else{
+        //Si no es un uno, lo apagamos
+        digitalWrite(13, LOW);
+      }
+    }
+
+}
