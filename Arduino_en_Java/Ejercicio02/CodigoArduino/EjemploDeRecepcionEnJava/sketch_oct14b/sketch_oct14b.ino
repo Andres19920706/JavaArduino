445d8b810abfc31ecac3289b56f18afba15b1805
Andres19920706 <arp00048@red.ujaen.es>
2017-10-14 21:48:48 +0200

    fin ejemplo 02

---
+++ Arduino_en_Java/Ejercicio02/CodigoArduino/EjemploDeRecepcionEnJava/sketch_oct14b/sketch_oct14b.ino
@@ -0,0 +1,17 @@
+int n = 0; //Variablen contadora
+void setup() {
+  //Establecemos la comunicacion con el puerto serie
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Creamos el mensaje
+  String output ="Mensaje numero ";
+  //Concatenmos
+  output += n;
+  //Enviamos el mensaje por el puerto serie
+  Serial.println(output);
+  n++; //Incrementos el numero del mensaje
+  delay(1000); //Esperamos para la siguiente transmision un 1 segundo
+
+}
